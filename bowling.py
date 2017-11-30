ba7608cdde2ac495cd04a2937fe7d40fac4f4315
mbalazs9001 <mbalazs9001@gmail.comgit config --global user.email mbalazs9001@gmail.comgit config --global user.email mbalazs9001@gmail.com>
2017-11-30 13:38:55 +0100

    get_value now makes only one check in the string "123456789"

--- bowling.py
+++ bowling.py
@@ -31,9 +31,7 @@ def score(game):
     return result
 
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
+    if char in "123456789":
         return int(char)
     elif char == 'X' or char == 'x' or char == '/':
         return 10
